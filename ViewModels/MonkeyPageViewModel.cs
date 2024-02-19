119fea4bbb0622ca001d2f282a46a0d51bb7a5f5
User <User@LAB3-3>
2024-02-19 13:09:40 +0200

    amir

--- ViewModels/MonkeyPageViewModel.cs
+++ ViewModels/MonkeyPageViewModel.cs
@@ -13,8 +13,12 @@ namespace MonkeysMVVM.ViewModels
 {
     public class MonkeyPageViewModel:ViewModel
     {
+        public Monkey SelectedMonkey { get; set; }
         public ObservableCollection<Monkey> Monkeys { get; set; }
         public ICommand LoadMonkeysCommand { get; private set; }
+        public ICommand NavigateToHomePage {  get; private set; }
+
+
         private bool isRefreshing;
         public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
         public MonkeyPageViewModel()
@@ -22,7 +26,19 @@ namespace MonkeysMVVM.ViewModels
             Monkeys = new ObservableCollection<Monkey>();
 
             LoadMonkeysCommand = new Command(async () => await LoadMonkeys());
+            NavigateToHomePage = new Command(async() => await GoHomePage());
         }
+
+        private async Task GoHomePage()
+        {
+            Dictionary<string,object> dic = new Dictionary<string,object>();
+            dic.Add("Monkey", SelectedMonkey);
+            await AppShell.Current.GoToAsync("ShowMonkey");
+           
+        }
+
+
+
 
         private async Task LoadMonkeys()
         {
